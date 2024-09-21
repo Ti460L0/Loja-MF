b04e8974aa35fd397cd12f0259e2d42a0f775ea6
Camilla Osório <camillaosorio1243@gmail.com>
2024-09-21 13:34:23 -0300

    Merge branch 'main' of https://github.com/avrox1911/Loja-MF

--- api/index.js
+++ api/index.js
@@ -1,32 +1,79 @@
-import dotenv from "dotenv";
 import express from "express";
+import bodyParser from "body-parser";
 import pg from "pg";
-import cors from "cors";
-dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const port = 3000;
 
-// Configuração do Pool do PostgreSQL usando variáveis de ambiente
-const { Pool } = pg;
-const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "postgres",
+  password: "8dt7CCwe",
+  port: 5432,
 });
 
-// Middleware
-app.use(cors());
-app.use(express.json());
+db.connect();
 
-// Endpoint de exemplo para obter dados
-app.get("/api", async (req, res) => {
- console.log(req.body);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("home.ejs");
 });
 
-// Iniciar o servidor
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register.ejs");
+});
+
+
+app.post("/register", async (req, res) => {
+  const email = req.body.username;
+  const password = req.body.password;
+
+  try {
+    const checkResult = await db.query("SELECT * FROM users WHERE email = $1", [
+      email,
+    ]);
+
+    if (checkResult.rows.length > 0) {
+      res.send("Email already exists. Try logging in.");
+    } else {
+      const result = await db.query(
+        "INSERT INTO users (email, password) VALUES ($1, $2)",
+        [email, password]
+      );
+      console.log(result);
+      res.render("secrets.ejs");
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  try {
+    const result = await db.query("SELECT * FROM users WHERE email = $1", [
+      username,
+    ]);
+
+    if (result.rows.length > 0 && result.rows[0].password === password) {
+      res.render("secrets.ejs");
+    } else {
+      res.send("<script>alert('Incorrect Username or Password');setTimeout(() => { location.href='/login'; }, 100);</script>");
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port http://localhost:${port}`);
 });
