198b3abd3687108e3654e794f2108a5cb5096363
avrox1911 <tiagosoad4ever@gmail.com>
2024-09-21 13:36:20 -0300

    registrar corrigido

--- src/components/screens/registrar.jsx
+++ src/components/screens/registrar.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import CadastroForm from "./forms/ClienteForm";
 import VestidosForm from "./forms/VestidosForm";
@@ -13,13 +12,6 @@ const handleSubmit = () => {
   .then(data => console.log(data))
   .catch(err => console.error(err));
 }
-=======
-import React, { useState } from "react";
-import { Link, useParams } from "react-router-dom";
-import ClienteForm from "../forms/ClienteForm";
-import VestidosForm from "../forms/VestidosForm";
-import AcessorioForm from "../forms/AcessorioForm";
->>>>>>> c166009b94cd11e4a716056e2292294f3f734c5a
 
 const Registrar = () => {
   const { valor } = useParams();
@@ -42,7 +34,6 @@ const Registrar = () => {
   };
 
   return (
-<<<<<<< HEAD
     <div className="flex flex-col max-w-7xl w-full mx-auto p-4 gap-4">
       <div className="bg-slate-800 p-4 w-full">
         <h2 className="text-xl font-bold text-center mb-4">Dados cliente</h2>
@@ -60,27 +51,9 @@ const Registrar = () => {
       </div>
       <button className="bg-slate-800 p-4 w-full" onClick={() => {handleSubmit()}}>Registrar</button>
     </div>
-=======
-    <>
-      <h1 className="text-2xl mb-4">Registrar: </h1>
-      <ul>
-        <li className="block mb-2">
-          <button onClick={() => changeScreen("cliente")}>Registrar cliente 🙋‍♀️</button>
-        </li>
-        <li className="block mb-2">
-          <button onClick={() => changeScreen("vestido")}>Registrar vestido 👗</button>
-        </li>
-        <li className="block mb-2">
-          <button onClick={() => changeScreen("acessorio")}>Registrar acessorio 👑</button>
-        </li>
-      </ul>
-      <div>{renderForm()}</div>
-    </>
->>>>>>> c166009b94cd11e4a716056e2292294f3f734c5a
   );
 };
 
-<<<<<<< HEAD
 export default Registrar;
 
 
@@ -90,6 +63,3 @@ export default Registrar;
 
 
 
-=======
-export default Registrar;
->>>>>>> adb7f2f0eb000970c9ce1056a91db96753e33a56
