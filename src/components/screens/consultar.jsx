d196994a0ff3cec0bd4299d05f132e1dddb3fef3
Camilla Osório <camillaosorio1243@gmail.com>
2024-09-24 14:24:58 -0300

    criando agenda

--- src/components/screens/consultar.jsx
+++ src/components/screens/consultar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import ClienteForm from "./forms/ClienteForm";
-import VestidosForm from "./forms/VestidosForm";
-import AcessorioForm from "./forms/AcessorioForm";
+import ClienteForm from "../screens/forms/cadastro/ClienteForm";
+import VestidosForm from "../screens/forms/cadastro/VestidosForm";
+import AcessorioForm from "../screens/forms/cadastro/AcessorioForm"
 
 const Consultar = () => {
   // Propriedades do formulário cliente
