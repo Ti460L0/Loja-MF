f87c83d0050ecd6d2f7eee16d2f210307a3808c8
avrox1911 <tiagosoad4ever@gmail.com>
2024-09-24 23:05:35 -0300

    busca de cliente ao digitar CPF

--- src/components/screens/MainPage.jsx
+++ src/components/screens/MainPage.jsx
@@ -1,61 +1,169 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ClienteForm from "./forms/cadastro/ClienteForm";
-import VestidoTabela from "./forms/consulta/TabelaVestido";
 
 const MainPage = () => {
-  const [formData, setFormData] = useState({});
+  const [formData, setFormData] = useState({
+    cliente_id: "",
+    nome: "",
+    email: "",
+    telefone: "",
+    cpf: "",
+    cep: "",
+    bairro: "",
+    endereco: "",
+  });
+  const [clienteExists, setClienteExists] = useState(false);
 
   const handleChange = (event) => {
-    setFormData({ ...formData, [event.target.name]: event.target.value });
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
   };
+
+  const searchCliente = () => {
+    if (formData.cpf.length === 11) {
+      // Realiza a busca se o CPF tiver exatamente 11 dígitos
+      try {
+        fetch(`http://ec2-18-216-195-241.us-east-2.compute.amazonaws.com:3000/clientes?cpf=${formData.cpf}`)
+          .then((response) => response.json())
+          .then((data) => {
+            if (data.length > 0) {
+              setFormData({
+                cliente_id: data[0].id,
+                nome: data[0].nome,
+                email: data[0].email,
+                telefone: data[0].telefone,
+                cpf: data[0].cpf,
+                cep: data[0].cep,
+                bairro: data[0].bairro,
+                endereco: data[0].endereco,
+              });
+              setClienteExists(true);
+            } else {
+              // Se o cliente não for encontrado, mantém o CPF e permite preencher os outros campos
+              setFormData({
+                cliente_id: "",
+                nome: "",
+                email: "",
+                telefone: "",
+                cpf: formData.cpf, // Mantém o CPF no campo
+                cep: "",
+                bairro: "",
+                endereco: "",
+              });
+              setClienteExists(false);
+            }
+          })
+          .catch((error) => console.error("Erro ao buscar cliente:", error));
+      } catch (error) {
+        console.error("Erro:", error);
+      }
+    } else {
+      // Reseta o formulário quando o CPF é apagado ou incompleto
+      setClienteExists(false);
+      setFormData({
+        cliente_id: "",
+        nome: "",
+        email: "",
+        telefone: "",
+        cpf: formData.cpf, // Mantém o CPF no campo
+        cep: "",
+        bairro: "",
+        endereco: "",
+      });
+    }
+  };
+
+  // Use useEffect para buscar o cliente quando o CPF tiver 11 dígitos
+  useEffect(() => {
+    if (formData.cpf.length === 11) {
+      searchCliente();
+    } else {
+      setClienteExists(false);
+    }
+  }, [formData.cpf]);
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(formData);
+    console.log("Formulário enviado:", formData);
+    // Adicione a lógica para cadastrar um novo cliente ou atualizar um existente
   };
 
   return (
     <div className="flex flex-col items-center justify-center w-full max-w-7xl mx-auto text-nowrap bg-slate-950 shadow-md rounded px-8 pt-6 pb-8 mb-4">
-      <form
-        className="w-full text-nowrap bg-slate-800 shadow-md rounded px-8 pt-6 pb-8 mb-4"
-        onSubmit={handleSubmit}
-      >
-        <ClienteForm formData={formData} handleChange={handleChange} />
-        <div className="flex flex-row justify-between mb-4 gap-4">
-          <div className="flex flex-col w-full md:w-1/2 lg:w-1/3">
-            <label className="block text-left mb-2" htmlFor="acessorio">
-              Acessorio
-            </label>
-            <select
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-slate-200 leading-tight focus:outline-none focus:shadow-outline"
-              id="acessorio"
-              name="acessorio"
-              value={formData.acessorio}
-              onChange={handleChange}
-            >
-              <option value="">Selecione um acessorio</option>
-              <option value="tiara">Tiara</option>
-              <option value="brinco">Brinco</option>
-              <option value="colar">Colar</option>
-              <option value="pulseira">Pulseira</option>
-            </select>
-          </div>
-          <VestidoTabela />
+      <form className="w-full text-nowrap bg-slate-800 shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit}>
+        <div className="flex flex-row justify-center mb-4">
+          <label className="text-left mb-2 mr-2" htmlFor="cpf">
+            CPF:
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-slate-200 leading-tight focus:outline-none focus:shadow-outline"
+            type="text"
+            id="cpf"
+            name="cpf"
+            value={formData.cpf}
+            onChange={handleChange}
+            placeholder="Digite o CPF (11 dígitos)"
+          />
         </div>
-        <div className="flex flex-row justify-end gap-2">
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            type="submit"
-          >
-            Confirmar
-          </button>
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            type="submit"
-          >
-            Cancelar
-          </button>
-        </div>
+
+        {/* Exibe o formulário do cliente se o CPF for encontrado */}
+        {clienteExists ? (
+          <>
+            <ClienteForm formData={formData} handleChange={handleChange} />
+            <div className="flex flex-row justify-end gap-2">
+              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit">
+                Confirmar
+              </button>
+              <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                type="button"
+                onClick={() =>
+                  setFormData({
+                    cliente_id: "",
+                    nome: "",
+                    email: "",
+                    telefone: "",
+                    cpf: "",
+                    cep: "",
+                    bairro: "",
+                    endereco: "",
+                  })
+                }
+              >
+                Cancelar
+              </button>
+            </div>
+          </>
+        ) : (
+          // Exibe o formulário vazio se o cliente não for encontrado, para criar um novo cliente
+          <>
+            <ClienteForm formData={formData} handleChange={handleChange} />
+            <p className="text-yellow-500">Cliente não encontrado. Preencha os dados para cadastrar um novo cliente.</p>
+            <div className="flex flex-row justify-end gap-2">
+              <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded" type="submit">
+                Cadastrar
+              </button>
+              <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                type="button"
+                onClick={() =>
+                  setFormData({
+                    cliente_id: "",
+                    nome: "",
+                    email: "",
+                    telefone: "",
+                    cpf: "",
+                    cep: "",
+                    bairro: "",
+                    endereco: "",
+                  })
+                }
+              >
+                Cancelar
+              </button>
+            </div>
+          </>
+        )}
       </form>
     </div>
   );
--- src/components/screens/forms/consulta/TabelaVestido.jsx
+++ src/components/screens/forms/consulta/TabelaVestido.jsx
@@ -1,22 +1,22 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-const VestidoTabela = () => {
+const TabelaVestido = ({ handleChange, multiple, onSelect }) => {
   const [vestidos, setVestidos] = useState([]);
-  const [codigoFiltro, setCodigoFiltro] = useState("");
-  const [vestidosFiltrados, setVestidosFiltrados] = useState([]);
+  const [vestidoSelecionado, setVestidoSelecionado] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchVestidos = async () => {
       try {
-        const response = await fetch('http://ec2-18-216-195-241.us-east-2.compute.amazonaws.com:3000/vestidos');
+        const response = await fetch(
+          `http://ec2-18-216-195-241.us-east-2.compute.amazonaws.com:3000/vestidos`
+        );
         if (response.ok) {
           const data = await response.json();
           setVestidos(data);
-          setVestidosFiltrados(data);
         } else {
-          throw new Error('Erro ao buscar vestidos');
+          throw new Error("Erro ao buscar vestidos");
         }
       } catch (error) {
         setError(error.message);
@@ -28,69 +28,81 @@ const VestidoTabela = () => {
     fetchVestidos();
   }, []);
 
-  useEffect(() => {
-    // Verificar se vestidos é um array e se codigoFiltro é uma string
-    if (Array.isArray(vestidos) && typeof codigoFiltro === 'string') {
-      const filteredVestidos = vestidos.filter(vestido => 
-        vestido.codigo && vestido.codigo.includes(codigoFiltro) // Verifica se vestido.codigo existe
-      );
-      setVestidosFiltrados(filteredVestidos);
-    }
-  }, [codigoFiltro, vestidos]);
-
-  const handleCodigoFiltroChange = (e) => {
-    setCodigoFiltro(e.target.value);
+  const handleSelect = (vestido_id) => {
+    const selectedVestido = vestidos.find((v) => v.id === vestido_id);
+    setVestidoSelecionado(selectedVestido);
+    onSelect(vestido_id);
   };
 
-  if (loading) {
-    return <div>Carregando vestidos...</div>;
-  }
-
-  if (error) {
-    return <div>Erro: {error}</div>;
-  }
-
   return (
-    <>
-      <div className='justify-center'>
-        <input
-          type="text"
-          value={codigoFiltro}
-          onChange={handleCodigoFiltroChange}
-          placeholder="Filtrar por código"
-          className="flex border rounded p-2 mb-4"
-        />
-        <table className="table-auto w-full border-collapse border border-gray-200">
-          <thead>
-            <tr>
-              <th className="border border-gray-300 px-4 py-2">Código</th>
-              <th className="border border-gray-300 px-4 py-2">Descrição</th>
-              <th className="border border-gray-300 px-4 py-2">Tamanho</th>
-              <th className="border border-gray-300 px-4 py-2">Cor</th>
-              <th className="border border-gray-300 px-4 py-2">Valor</th>
-            </tr>
-          </thead>
-          <tbody>
-            {vestidosFiltrados.length > 0 ? (
-              vestidosFiltrados.map(vestido => (
-                <tr key={vestido.codigo}>
-                  <td className="border border-gray-300 px-4 py-2">{vestido.codigo}</td>
-                  <td className="border border-gray-300 px-4 py-2">{vestido.descricao}</td>
-                  <td className="border border-gray-300 px-4 py-2">{vestido.tamanho}</td>
-                  <td className="border border-gray-300 px-4 py-2">{vestido.cor}</td>
-                  <td className="border border-gray-300 px-4 py-2">{`R${vestido.valor}`}</td>
-                </tr>
-              ))
-            ) : (
+    <div>
+      {multiple ? (
+        <div className="overflow-x-auto">
+          <table className="table-auto w-full text-left">
+            <thead className="bg-slate-600">
               <tr>
-                <td colSpan="5" className="border border-gray-300 px-4 py-2 text-center">Nenhum vestido encontrado</td>
+                <th className="px-4 py-2">Código</th>
+                <th className="px-4 py-2">Modelo</th>
+                <th className="px-4 py-2">Tamanho</th>
+                <th className="px-4 py-2">Cor</th>
+                <th className="px-4 py-2">Valor</th>
               </tr>
-            )}
-          </tbody>
-        </table>
-      </div>
-    </>
+            </thead>
+            <tbody className="bg-slate-800">
+              {vestidos.map((v) => (
+                <tr
+                  key={v.vestido_id}
+                  className={`hover:bg-sky-800 ${
+                    vestidoSelecionado && vestidoSelecionado.id === v.id
+                      ? "bg-blue-500"
+                      : ""
+                  }`}
+                  onClick={() => handleSelect(v.id)}
+                >
+                  <td className="border px-4 py-2">{v.codigo}</td>
+                  <td className="border px-4 py-2">{v.modelo}</td>
+                  <td className="border px-4 py-2">{v.tamanho}</td>
+                  <td className="border px-4 py-2">{v.cor}</td>
+                  <td className="border px-4 py-2">R${v.valor}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      ) : (
+        <div className="flex flex-col w-full">
+          <label className="mb-2" htmlFor="vestido-select">
+            Escolha um vestido:
+          </label>
+          <input
+            className="px-4 py-2 border-2 border-gray-300 rounded-lg w-full"
+            type="search"
+            id="vestido-select"
+            onChange={(e) => handleSelect(Number(e.target.value))}
+            placeholder="Digite o código do vestido"
+            list="vestidos"
+          />
+          <datalist id="vestidos">
+            {vestidos.slice(0, 10).map((v) => (
+              <option key={v.vestido_id} value={v.codigo}>
+                {v.modelo}, {v.status}
+              </option>
+            ))}
+          </datalist>
+          {vestidoSelecionado && (
+            <div className="mt-4 bg-slate-800 p-4 rounded-lg">
+              <p className="font-bold">Modelo: {vestidoSelecionado.modelo}</p>
+              <p className="mt-2">Tamanho: {vestidoSelecionado.tamanho}</p>
+              <p className="mt-2">Cor: {vestidoSelecionado.cor}</p>
+              <p className="mt-2">Status: {vestidoSelecionado.status}</p>
+              <p className="mt-2">Valor: R${vestidoSelecionado.valor}</p>
+            </div>
+          )}
+        </div>
+      )}
+    </div>
   );
 };
 
-export default VestidoTabela;
+export default TabelaVestido;
+
