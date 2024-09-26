2863572dc109a69a50ce5e5e4666bb79af3e958b
avrox1911 <tiagosoad4ever@gmail.com>
2024-09-26 17:46:49 -0300

    agenda pegando valores do database

--- src/components/screens/Calendario.jsx
+++ src/components/screens/Calendario.jsx
@@ -12,7 +12,7 @@ const CalendarComponent = ({ onDateSelect }) => {
 
     const calendarDays = [];
     for (let i = 0; i < firstDayOfMonth; i++) {
-      calendarDays.push(<div key={`empty-${i}`} className="empty-day" />); // Dias vazios no início
+      calendarDays.push(<div key={`empty-${i}`} className="py-2 px-4" />); // Dias vazios no início
     }
 
     for (let day = 1; day <= daysInMonth; day++) {
@@ -20,7 +20,7 @@ const CalendarComponent = ({ onDateSelect }) => {
       calendarDays.push(
         <div
           key={day}
-          className="calendar-day"
+          className="w-12 h-12 py-2 px-4 rounded-lg text-center transition duration-800 ease-in-out hover:bg-gray-800 hover:text-white  cursor-pointer"
           onClick={() => handleDateClick(date)}
         >
           {day}
@@ -38,22 +38,43 @@ const CalendarComponent = ({ onDateSelect }) => {
   // Função para mudar o mês
   const changeMonth = (direction) => {
     setCurrentDate((prevDate) => {
-      const newDate = new Date(prevDate.setMonth(prevDate.getMonth() + direction));
-      return newDate;
+      const newDate = new Date(prevDate);
+      newDate.setMonth(newDate.getMonth() + direction); // Muda o mês da nova data
+      return newDate; // Retorna a nova data
     });
   };
 
   return (
-    <div className="calendar-container">
-      <div className="calendar-header">
-        <button onClick={() => changeMonth(-1)}>Anterior</button>
-        <h2>{currentDate.toLocaleDateString('pt-BR', { month: 'long', year: 'numeric' })}</h2>
-        <button onClick={() => changeMonth(1)}>Próximo</button>
-      </div>
-      <div className="calendar-grid">
-        {renderDaysInMonth()}
-      </div>
+    <div className="max-w-xl bg-gray-950 p-4 rounded-lg shadow-md">
+    <div className="flex items-center justify-between mb-4">
+      <button 
+        onClick={() => changeMonth(-1)} 
+        className="bg-gray-700 text-white px-4 py-2 rounded hover:bg-gray-600 transition"	
+      >
+        Anterior
+      </button>
+      <h2 className="text-xl font-bold text-white">
+        {currentDate.toLocaleDateString('pt-BR', { month: 'long', year: 'numeric' })}
+      </h2>
+      <button 
+        onClick={() => changeMonth(1)} 
+        className="bg-gray-700 text-white px-4 py-2 rounded hover:bg-gray-600 transition"
+      >
+        Próximo
+      </button>
     </div>
+    <div className="grid grid-cols-7 gap-2 mb-2">
+      {/* Nomes dos dias */}
+      {["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"].map((day, index) => (
+        <div key={index} className="text-center text-white font-semibold">{day}</div>
+      ))}
+    </div>
+    <div className="grid grid-cols-7 gap-2">
+      {renderDaysInMonth()}
+    </div>
+  </div>
+  
+
   );
 };
 
